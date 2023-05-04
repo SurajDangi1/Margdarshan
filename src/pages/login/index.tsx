0d2d518affaa4b6194d92273b042e71487934f81
SurajDangi1 <122509651+SurajDangi1@users.noreply.github.com>
2023-05-04 19:30:25 +0530

    login done

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import Image from "next/image";
+import Link from "next/link";
 
 interface FormValues {
   email: string;
@@ -25,89 +27,86 @@ const Login = () => {
       .min(8, 'Password must be at least 8 characters')
   });
 
-  const url = "https://localhost:9000/auth/login";
-
-  const handleSubmit = async (values: FormValues) => {
+  const url = process.env.LOGIN_URL as string;
+  console.log(url)
+  const handleSubmit = async (values: FormValues): Promise<void> => {
     try {
-      const response = await axios.post(url, values);
-      console.log('Login successful!', response.data);
+      const response = await axios.post("http://localhost:9000/auth/login", values, { withCredentials: true });
     } catch (error) {
-      console.error('Login failed!', error);
+      console.error("Error:", Response);
     }
   };
-
+  
   return (
     <div className="grid grid-cols-2">
       <div>
-        <section className=" ">
-          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-
-            <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-                  {({ errors, touched, isSubmitting }) => (
-                    <Form className="space-y-4 md:space-y-6" action="#">
-                      <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-grey-900">
-                          Your email
-                        </label>
-                        <Field
-                          type="email"
-                          name="email"
-                          id="email"
-                          placeholder="name@company.com"
-                          className={`bg-grey-50 border ${errors.email && touched.email ? 'border-red-600' : 'border-grey-300'
-                            } text-grey-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
-                        />
-                        <ErrorMessage name="email" className="text-red-600" component="div" />
-                      </div>
-                      <div>
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-grey-900">
-                          Password
-                        </label>
-                        <Field
-                          type="password"
-                          name="password"
-                          id="password"
-                          placeholder="••••••••"
-                          className={`bg-grey-50 border ${errors.password && touched.password ? 'border-red-600' : 'border-grey-300'
-                            } text-grey-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
-                        />
-                        <ErrorMessage name="password" className="text-red-600" component="div" />
-                      </div>
-                      <div className="flex items-center justify-between">
-                        <div className="flex items-start">
-                          <div className="flex items-center h-5">
-                            <Field
-                              type="checkbox"
-                              id="remember"
-                              name="remember"
-                              className="w-4 h-4 border border-grey-300 rounded bg-grey-50 focus:ring-3 focus:ring-primary-300 dark:bg-grey-700 dark:border-grey-600 dark:focus:ring-primary-600 dark:ring-offset-grey-800"
-                            />
-                          </div>
-                          <div className="ml-3 text-sm">
-                            <label htmlFor="remember" className="text-grey-900">
-                              Remember me
-                            </label>
-                          </div>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+                {({ errors, touched, isSubmitting }) => (
+                  <Form className="space-y-4 md:space-y-6" action="#">
+                    <div>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-grey-900">
+                        Your email
+                      </label>
+                      <Field
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="name@company.com"
+                        className={`bg-grey-50 border ${errors.email && touched.email ? 'border-red-600' : 'border-grey-300'
+                          } text-grey-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                      />
+                      <ErrorMessage name="email" className="text-red-600" component="div" />
+                    </div>
+                    <div>
+                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-grey-900">
+                        Password
+                      </label>
+                      <Field
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        className={`bg-grey-50 border ${errors.password && touched.password ? 'border-red-600' : 'border-grey-300'
+                          } text-grey-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                      />
+                      <ErrorMessage name="password" className="text-red-600" component="div" />
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-start">
+                        <div className="flex items-center h-5">
+                          <Field
+                            type="checkbox"
+                            id="remember"
+                            name="remember"
+                            className="w-4 h-4 border border-grey-300 rounded bg-grey-50 focus:ring-3 focus:ring-primary-300 dark:bg-grey-700 dark:border-grey-600 dark:focus:ring-primary-600 dark:ring-offset-grey-800"
+                          />
                         </div>
-                        <a href="#" className="text-sm font-medium text-cherry-300 hover:underline dark:text-primary-500">
-                          Forgot password?
-                        </a>
+                        <div className="ml-3 text-sm">
+                          <label htmlFor="remember" className="text-grey-900">
+                            Remember me
+                          </label>
+                        </div>
                       </div>
-                      <button type="submit" disabled={isSubmitting} className="w-full text-black bg-cherry-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in</button>
-                      <p className="text-sm font-light text-grey-900">
-                        Don’t have an account yet? <a href="/signup" className="font-medium text-cherry-300 hover:underline dark:text-primary-500">Sign up</a>
-                      </p>
-                    </Form>
-                  )}
-                </Formik>
-              </div>
+                      <a href="#" className="text-sm font-medium text-cherry-300 hover:underline dark:text-primary-500">
+                        Forgot password?
+                      </a>
+                    </div> 
+                    <button type="submit" disabled={isSubmitting} className="w-full text-black bg-cherry-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in</button>
+                    <p className="text-sm font-light text-grey-900">
+                      Don’t have an account yet? <a href="/signup" className="font-medium text-cherry-300 hover:underline dark:text-primary-500">Sign up</a>
+                    </p>
+                  </Form>
+                )}
+              </Formik>
             </div>
           </div>
-        </section>
+        </div>
+
       </div>
-      <div className="pt-24 bg-grey-300">
+      <div className="pt-24 bg-grey-300 hidden lg:block">
         <Image src='/Scholarship-dum-images/log-in.jpg' className="rounded-xl pl-10 border-x-4 border-y-4" alt='img' height={90} width={1000}></Image>
       </div>
     </div>
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -47,9 +47,16 @@ const validationSchema = yup.object().shape({
   // postalCode: yup.string().required("Postal code is required"),
 });
 
-export default function SignUp() {
+
+
+const SignUp = () => {
   const url = "https://localhost:9000/auth/signup";
-  const handleSubmit = async (values: FormValues) => {
+
+
+ 
+  const [selectedCourse, setSelectedCourse] = useState("");
+
+  const handleSubmit = async (values: FormValues): Promise<void> => {
     console.log(values);
 
     try {
@@ -59,27 +66,26 @@ export default function SignUp() {
       console.error('Sign Up failed!', error);
     }
   }
-
-
+ 
   return (
     <div className="grid grid-cols-2">
       <div className="pt-24 mb-20 pl-10 pr-10 ">
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-          {({ errors, touched, isSubmitting }) => (
-            <Form >
-              <div className="border-grey-900/10 pl-2 pr-2 rounded-xl pb-12 border">
+        <div className="border-grey-900 pl-2 pr-2 rounded-xl pb-12 border">
+          <Formik initialValues={initialValues}   validationSchema={validationSchema}
+            onSubmit={handleSubmit}>
+            {({ errors, touched, isSubmitting }) => (
+              <Form>
                 <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 mb-5">
                   <div className="sm:col-span-3">
                     <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-grey-900">
                       Full name
                     </label>
-                    <div className="mt-2">
+                    <div className="">
                       <Field
                         type="text"
                         name="firstName"
                         id="firstName"
-                     
-                        className={`block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ${errors.firstName && touched.firstName ? "ring-red-500 placeholder:text-grey-400" : "ring-grey-500 placeholder:text-grey-400"
+                        className={`block w-full px-1 rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ${errors.firstName && touched.firstName ? "ring-red-500 placeholder:text-grey-400" : "ring-grey-500 placeholder:text-grey-400"
                           } sm:text-sm sm:leading-6`}
                       />
                       <ErrorMessage name="firstName" component="div" className="text-red-500 text-xs mt-1" />
@@ -90,13 +96,12 @@ export default function SignUp() {
                     <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-grey-900">
                       Last name
                     </label>
-                    <div className="mt-2">
+                    <div className="">
                       <Field
                         type="text"
                         name="lastName"
                         id="lastName"
-                      
-                        className={`block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ${errors.firstName && touched.firstName ? "ring-red-500 placeholder:text-grey-400" : "ring-grey-500 placeholder:text-grey-400"
+                        className={`block w-full px-1 rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ${errors.firstName && touched.firstName ? "ring-red-500 placeholder:text-grey-400" : "ring-grey-500 placeholder:text-grey-400"
                           } sm:text-sm sm:leading-6`}
                       />
                       <ErrorMessage name="lastName" component="div" className="text-red-500 text-xs mt-1" />
@@ -105,40 +110,103 @@ export default function SignUp() {
 
 
                   <div className="sm:col-span-3">
-                    <label htmlFor="course" className="block text-sm font-medium leading-6 text-grey-900">
+                    <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-grey-900">
                       Education (currently pursuing)
                     </label>
-                    <div className="mt-2">
+                    <Field
+                      as="select"
+                      id="course"
+                      name="course"
+                      autoComplete="course-name"
+                      className="block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      onChange={(e: any) => setSelectedCourse(e.target.value)}
+                    >
+                      <option value="">Select an option</option>
+                      <option value="10th">10th</option>
+                      <option value="12th">12th</option>
+                      <option value="Post Graduate">Post Graduate</option>
+                      <option value="Under Graduate">Under Graduate</option>
+                    </Field>
+                  </div>
+
+
+                  {selectedCourse === "12th" && (
+                    <div className="sm:col-span-3">
+                      <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-grey-900">
+                        12th Stream
+                      </label>
                       <Field
                         as="select"
-                        id="course"
-                        name="course"
-                        autoComplete="course-name"
+                        id="12th"
+                        name="12th"
+                        autoComplete="under-graduate-course"
                         className="block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                       >
-                        <option value="">Select an option</option>
-                        <option value="10th">10th</option>
-                        <option value="12th(Arts)">12th(Arts)</option>
-                        <option value="12th(Commerce)">12th(Commerce)</option>
-                        <option value="12th(Science)">12th(Science)</option>
-                        <option value="Post Graduate">Post Graduate</option>
-                        <option value="Under Graduate">Under Graduate</option>
+                        <option value="">Arts</option>
+                        <option value="B.Tech">Commerce</option>
+                        <option value="B.A">Science</option>
+
                       </Field>
-                      <ErrorMessage name="course" component="div" className="text-red-500 text-sm mt-1" />
+                      <ErrorMessage name="12th" component="div" className="text-red-500 text-sm mt-1" />
                     </div>
-                  </div>
+                  )}
+
+                  {selectedCourse === "Post Graduate" && (
+
+                    <div className="sm:col-span-3">
+                      <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-grey-900">
+                        Post Graduate Course
+                      </label>
+                      <Field
+                        as="select"
+                        id="postGraduateCourse"
+                        name="postGraduateCourse"
+                        autoComplete="post-graduate-course"
+                        className="block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      >
+                        <option value="">Select a course</option>
+                        <option value="MBA">MBA</option>
+                        <option value="M.Tech">M.Tech</option>
+                        <option value="M.Sc">M.Sc</option>
+                      </Field>
+                      <ErrorMessage name="postGraduateCourse" component="div" className="text-red-500 text-sm mt-1" />
+                    </div>
+                  )}
+
+                  {selectedCourse === "Under Graduate" && (
+                    <div className="sm:col-span-3">
+                      <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-grey-900">
+                        Under Graduate Course
+                      </label>
+                      <Field
+                        as="select"
+                        id="underGraduateCourse"
+                        name="underGraduateCourse"
+                        autoComplete="under-graduate-course"
+                        className="block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      >
+                        <option value="">Select an course</option>
+                        <option value="B.Tech">B.Tech</option>
+                        <option value="B.A">B.A</option>
+                        <option value="B.Com">B.Com</option>
+                        <option value="B.Sc">B.Sc</option>
+                      </Field>
+                      <ErrorMessage name="underGraduateCourse" component="div" className="text-red-500 text-sm mt-1" />
+                    </div>
+                  )}
+
 
 
                   <div className="sm:col-span-3">
                     <label htmlFor="course" className="block text-sm font-medium leading-6 text-grey-900">
                       Gender
                     </label>
-                    <div className="mt-2">
+                    <div className="">
                       <Field
                         as="select"
                         id="gender"
                         name="gender"
-                      
+
                         className="block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                       >
                         <option value="">Select an option</option>
@@ -155,13 +223,13 @@ export default function SignUp() {
                     <label htmlFor="email" className="block text-sm font-medium leading-6 text-grey-900">
                       Email
                     </label>
-                    <div className="mt-2">
+                    <div className="">
                       <Field
                         type="email"
                         name="email"
                         id="email"
-                     
-                        className="block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 placeholder:text-grey-400 sm:text-sm sm:leading-6"
+
+                        className="block w-full px-1 rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 placeholder:text-grey-400 sm:text-sm sm:leading-6"
                       />
                       <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
                     </div>
@@ -171,14 +239,14 @@ export default function SignUp() {
                     <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-grey-900">
                       Password
                     </label>
-                    <div className="mt-2">
+                    <div className="">
                       <Field
                         type="password"
                         name="password"
                         id="password"
                         placeholder="••••••••"
                         className={`bg-grey-50 border ${errors.password && touched.password ? 'border-red-600' : 'border-grey-300'
-                          } text-grey-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                          } text-grey-900 px-1 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full py-1.5`}
                       />
                       <ErrorMessage name="password" className="text-red-600" component="div" />
                     </div>
@@ -186,18 +254,17 @@ export default function SignUp() {
 
 
 
-                  <div className="col-span-full">
+                  <div className="sm:col-span-3">
                     <label htmlFor="dateOfBirth" className="block text-sm font-medium leading-6 text-grey-900">
                       DOB
                     </label>
-                    <div className="mt-2">
+                    <div className="">
                       <Field
                         type="date"
                         name="dateOfBirth"
                         id="dateOfBirth"
-                       
-                        className={`block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 placeholder:text-grey-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.dateOfBirth && touched.dateOfBirth ? 'border-red-500' : ''
-                          }`}
+                        className={`bg-grey-50 border ${errors.password && touched.password ? 'border-red-600' : 'border-grey-300'
+                          } text-grey-900 px-1 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full py-1.5`}
                       />
                       {errors.dateOfBirth && touched.dateOfBirth ? (
                         <div className="text-red-500 text-sm mt-1">{errors.dateOfBirth}</div>
@@ -209,13 +276,13 @@ export default function SignUp() {
                     <label htmlFor="city" className="block text-sm font-medium leading-6 text-grey-900">
                       City
                     </label>
-                    <div className="mt-2">
+                    <div className="">
                       <Field
                         type="text"
                         name="city"
                         id="city"
-                       
-                        className={`block w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 placeholder:text-grey-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.city && touched.city ? 'border-red-500' : ''
+
+                        className={`block px-1 w-full rounded-md border-0 py-1.5 text-grey-900 shadow-sm ring-1 ring-inset ring-grey-500 placeholder:text-grey-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.city && touched.city ? 'border-red-500' : ''
                           }`}
                       />
                       {errors.city && touched.city ? <div className="text-red-500 text-sm mt-1">{errors.city}</div> : null}
@@ -226,44 +293,36 @@ export default function SignUp() {
                     <label htmlFor="region" className="block text-sm font-medium leading-6 text-grey-900">
                       State / Province
                     </label>
-                    <div className="mt-2">
+                    <div className="">
                       <Field
                         type="text"
                         name="state"
                         id="state"
                         className={`bg-grey-50 border ${errors.password && touched.password ? 'border-red-600' : 'border-grey-300'
-                          } text-grey-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                          } text-grey-900 px-1 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full py-1.5`}
                       />
                       <ErrorMessage name="state" className="text-red-600" component="div" />
                     </div>
                   </div>
-                  {/* <div className="sm:col-span-2">
-                    <label htmlFor="postalCode" className="block text-sm font-medium leading-6 text-grey-900">
-                      ZIP / Postal code
-                    </label>
-                    <div className="mt-2">
-                      <Field
-                        type="number"
-                        name="postalCode"
-                        id="postalCode"
-                        autoComplete="postal-code"
-                        className={`bg-grey-50 border ${errors.password && touched.password ? 'border-red-600' : 'border-grey-300'
-                          } text-grey-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
-                      />
-                      <ErrorMessage name="postalCode" className="text-red-600" component="div" />
-                    </div>
-                  </div> */}
+                 
                 </div>
-                <button type="submit" disabled={isSubmitting} className="w-full text-black bg-cherry-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign Up</button>
-              </div>
-            </Form>
-          )}
-        </Formik>
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="w-full text-black bg-cherry-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign Up
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
-      <div className="pt-36">
+      <div className="pt-36 hidden lg:block">
         <Image src='/Scholarship-dum-images/img-1.jpg' className="rounded" alt='img-1' width={1000} height={100}></Image>
       </div>
-    </div>
+   </div>
   )
 };
 
+export default SignUp;
